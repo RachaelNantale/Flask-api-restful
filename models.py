5047477973edc9f05a596bcb2b90fd5d0b23da44
RachaelNantale <rachaelminani42@gmail.com>
2018-05-31 12:38:14 +0300

    Add models

---
+++ models.py
@@ -0,0 +1,23 @@
+class a_request:
+    def __init__(self, _id, _request, _type):
+        self._request = _request
+        self._type = _type
+        self._id = _id
+    
+    def get_id(self):
+        return self._id
+    
+    def get_request(self):
+        return self._request
+
+    def get_type(self):
+        return self._type
+
+
+maintance_requests = [
+        
+    
+]
+#-------------------------------------------------------------------------------------
+# 
+#-------------------------------------------------------------------------------------
