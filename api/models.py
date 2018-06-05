044b46bc341dc00b2af78170dc8079c275372a0c
RachaelNantale <rachaelminani42@gmail.com>
2018-06-05 05:01:08 +0300

    Add user model

--- api/models.py
+++ api/models.py
@@ -1,63 +1,49 @@
-# class a_request:
-#     def __init__(self, _id, _request, _type):
-#         self._request = _request
-#         self._type = _type
-#         self._id = _id
+#create class for
+class a_request:
+    def __init__(self, _id, _request, _type):
+        self._request = _request
+        self._type = _type
+        self._id = _id
     
-#     def get_id(self):
-#         return self._id
+    def get_id(self):
+        return self._id
     
-#     def get_request(self):
-#         return self._request
+    def get_request(self):
+        return self._request
 
-#     def get_type(self):
-#         return self._type
+    def get_type(self):
+        return self._type
+
+    def get_dict(self):
+        return{'_id':self._id,
+        '_type':self._type,
+        '_request':self._request}
 
 
 maintance_requests = [
-   {
-        "id":"1",
-        "requests": "computer not working",
-        "type":"repair"
-    },
-    {
-        "id":"2",
-        "requests": "computer  missing",
-        "type":"repair"
-    },
-    {
-        "id":"3",
-        "requests": "software malware",
-        "type":"maintenance"
-    },
-    {
-        "id":"4",
-        "requests": "computer crashed",
-        "type":"repair",
-    }
-        
-    
+a_request('1', 'computer not working','repair')
 ]
+#create class for users
+class a_user:
+    def __init__(self, _id, _username, _password):
+        self._id = _id
+        self._username = _username
+        self._password = _password
+    
+    def get_id(self):
+        return self._id
+    
+    def get_username(self):
+        return self._username
+
+    def get_password(self):
+        return self._password
+
+
+
+
 
 user_list = [
-    {
-        "id":"1",
-        "username": "rachaelN",
-        "password":"123abc"
-    },
-     {
-        "id":"1",
-        "username": "username2",
-        "password":"123abc"
-    },
-     {
-        "id":"1",
-        "username": "username3",
-        "password":"123abc"
-    },
-     {
-        "id":"2",
-        "username": "username4",
-        "password":"123abc"
-    },
+a_user('1','rachaelN','123abc')
+   
 ]
