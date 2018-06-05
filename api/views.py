14b3789624ebdbdc51032449b75bf1a9f6ffc053
RachaelNantale <rachaelminani42@gmail.com>
2018-06-05 06:24:16 +0300

    Add authentication

--- api/views.py
+++ api/views.py
@@ -1,145 +1,196 @@
-from flask import Flask, request, jsonify,abort, Blueprint
+from flask import Flask, request, jsonify,abort, Blueprint, make_response
 import json
 from api.models import a_request, a_user
 from api.models import maintance_requests, user_list
 import jwt
 import datetime
 import uuid
-from werkzerg.security import generate_password_hash, check_hash
+from werkzeug.security import generate_password_hash
+from functools import wraps
+
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "nanana"
 
 my_app = Blueprint('my_app',__name__)
+#create a token header
+def token_required(f):
+	@wraps(f)
+	def decorated(*args, **kwargs):
+		token = None
 
-    
+		if 'x-access-token' in request.headers:
+			token = request.headers['x-access-token']
+		if not token:
+			return jsonify({'message':'Token is missing'}),401
+
+		try:
+			data =jwt.decode(token, app_config('SECRET_KEY'))
+			current_user = a_user.query.filter_by(_id=data['id'])
+		except:
+			return jsonify({'message':'Token is missing'}),401
+		return f(current_user, *args, **kwargs)
+	return decorated
+
+	
 #create a new request
 @my_app.route('/api/v1/users/requests' ,methods= ['POST'])
-def create_request():
+@token_required
 
-    """
-        This endpoint creates a maintance request ticket
-    """
+def create_request(current_user):
+
+	"""
+		This endpoint creates a maintance request ticket
+	"""
+	if not current_user.admin:
+		return jsonify({'message: User can not collect all requests'})
+	
    
-    data = request.get_json()
+	data = request.get_json()
    
-    _requests = data.get('requests'),
-    if not _requests or _requests == '':
-        return jsonify ({'message': 'Missing information. Please fill in'}), 400
-    _types = data.get('type')
-    if not _types or _types =='':
-         return jsonify ({'message': 'Missing infor Please fill in'}), 400
+	_requests = data.get('requests'),
+	if not _requests or _requests == '':
+		return jsonify ({'message': 'Missing information. Please fill in'}), 400
+	_types = data.get('type')
+	if not _types or _types =='':
+		 return jsonify ({'message': 'Missing infor Please fill in'}), 400
    
-    try:
-        if isinstance(data['requests'], str) and isinstance(data['type'], str):
-            id = len(maintance_requests)
-            id += 1 
-            Request =a_request(id,data['requests'],data['type'] )
-            maintance_requests.append(Request)  
-        return jsonify(Request.get_dict()), 201
-    #Add an Attribut error to catch the errors
-    except AttributeError:
-        return jsonify({
-            'status': 'FAIL',
-            'message': 'Failed to create a request. Invalid data'
-        }), 400
+	try:
+		if isinstance(data['requests'], str) and isinstance(data['type'], str):
+			id = len(maintance_requests)
+			id += 1 
+			Request =a_request(id,data['requests'],data['type'] )
+			maintance_requests.append(Request)  
+		return jsonify(Request.get_dict()), 201
+	#Add an Attribut error to catch the errors
+	except AttributeError:
+		return jsonify({
+			'status': 'FAIL',
+			'message': 'Failed to create a request. Invalid data'
+		}), 400
 
 #create an api endpoint for modifying requests
 @app.route('/api/v1/users/requests/<id>' ,methods= ['PUT'])
-def modify_request(id):
-    data = request.get_json()
-    """
-    This endpoint modifies a request 
-    """
-    _requests = data.get('requests'),
-    _types = data.get('type')
-    if not _requests or _requests == ' ':
-        return jsonify ({'message': 'Missing information. Please fill in'}), 400
-    _types = data.get('type')
-    if not _types or _types ==' ':
-         return jsonify ({'message': 'Missing infor Please fill in'}), 400
+@token_required
+
+def modify_request(id, current_user):
+	if not current_user.admin:
+		return jsonify({'message: User can not collect all requests'})
+	
+	
+	data = request.get_json()
+	"""
+	This endpoint modifies a request 
+	"""
+	_requests = data.get('requests'),
+	_types = data.get('type')
+	if not _requests or _requests == ' ':
+		return jsonify ({'message': 'Missing information. Please fill in'}), 400
+	_types = data.get('type')
+	if not _types or _types ==' ':
+		 return jsonify ({'message': 'Missing infor Please fill in'}), 400
   
-    
-    #try:
-    if isinstance(data['requests'], str) and isinstance(data['type'], str):
-        counter = 0
-    for item in maintance_request:
-        if id == item.get_id():
-            maintance_request[counter]
-        return
-        counter = counter +1
-        a_request('id', 'request','type'),200
+	
+	#try:
+	if isinstance(data['requests'], str) and isinstance(data['type'], str):
+		
+		counter = 0
+	for item in maintance_request:
+		if id == item.get_id():
+			maintance_request[counter]
+		return
+		counter = counter +1
+		a_request('id', 'request','type'),200
 #except AttributeError, IndexError):
-        return jsonify({
-            'status': 'FAIL',
-            'message': 'Failed to modify a request. Invalid data'
-        }), 400
+		return jsonify({
+			'status': 'FAIL',
+			'message': 'Failed to modify a request. Invalid data'
+		}), 400
 
 #create API endpoints for fetching all requests
 @app.route('/api/v1/users/requests',methods= ['GET'])
-def fetch_all_requests():
-    count = len(maintance_requests) 
+@token_required
+
+def fetch_all_requests(current_user):
+	if not current_user.admin:
+		return jsonify({'message: User can not collect all requests'})
+	
+
+	count = len(maintance_requests) 
   
 
 
-    return jsonify({
-        'status': 'OK',
-        'message': 'here are all your requests',
-        'request_number': count,
-        'requests': maintance_requests
+	return jsonify({
+		'status': 'OK',
+		'message': 'here are all your requests',
+		'request_number': count,
+		'requests': maintance_requests
 
-    }), 200
+	}), 200
 
 #create API endpoints for fecthind a single id
 @app.route('/api/v1/users/requests/<requestID>/', methods = ['GET'])
-def fetch_request_id(requestID):
-    fetch_requests = []
-    data = maintance_requests
-    if int(requestID) > len(data):
-        return jsonify({
-            'status':'Fail',
-            'message':'ID not found. Please add a valid ID'
+@token_required
 
-        }),400
-    obj = data[int(requestID)-1]
-    fetch_requests.append(obj)
+def fetch_request_id(requestID, current_user):
+	if not current_user.admin:
+		return jsonify({'message: User can not collect all requests'})
+	
+	
 
-    return jsonify({
-        'status':'Success',
-        'request':fetch_requests
-    })
+	fetch_requests = []
+	data = maintance_requests
+	if int(requestID) > len(data):
+		return jsonify({
+			'status':'Fail',
+			'message':'ID not found. Please add a valid ID'
 
-@app.route('/auth/login', methods = ['POST'])
+		}),400
+	obj = data[int(requestID)-1]
+	fetch_requests.append(obj)
+
+	return jsonify({
+		'status':'Success',
+		'request':fetch_requests
+	})
+	
+@app.route('/auth/login')
+
 def user_login():
-    try:
-        payload = {
-            'exp': datetime.datetime.utcnow() + datetime.timedelta(days=0, seconds=5),
-            'iat': datetime.datetime.utcnow(),
-            'username': request.get_json()["username"]
-        }
-        token = jwt.encode(payload,app.config.get('SECRET_KEY'),algorithm='HS256')
-        return jsonify({"token":token.decode('UTF-8')})
-    except Exception as e:
-        return e
+	auth = request.authorization
+
+	if not auth or not auth.username or not auth.password:
+		return make_response('could not verify',401,{'WWW-Aunthenticate':'Basic realm="Login required"'})
+
+	user = a_user.filter_by(name=auth.username).first()
+
+	if not user:
+		return jsonify({'message':'No user Found'})
+
+	if check_password_hash(user.password, auth.password):
+		payload = {
+		'_id':'a_user._id',
+		'exp':datetime.datetime.utcnow() + datetime.timedelta(minutes=30)
+
+		}
+		token = jwt.encode({payload,app.config.get(SECRET_KEY)})
+		return jsonify({'token':token.decode(UTF-8)})
+	return make_response('Could not verify',401,{'WWW-Aunthenticate':'Basic realm="Login required"'})
+	
+
 
 
 @app.route('/api/v1/users', methods=['POST'])
-def create_user():
-    data = request.get_json()
-    hashed_password = generate_password_hash(data['password'], method='sha256')
-    new_user = User (public_id =str(uuid.uuid4()), name=data ['name'],password,admin=False)   
+@token_required
 
-
-# @auth.get_password
-# def get_password():
-#     if 'username' == [user_list]['username']:
-#         return 'python'
-#     return None
-
-# @auth.error_handler
-# def unauthorized():
-#     return make_response(jsonify({'error': 'Unauthorized access'}), 401)
+def create_user(current_user):
+	if not current_user.admin:
+		return jsonify({'message: User can not collect all requests'})
+	
+	data = request.get_json()
+	hashed_password = generate_password_hash(data['password'], method='sha256')
+	new_user = a_user(_id =str(uuid.uuid4()), name=data ['username'],password=data['hashed_password'],admin=False)   
 
 
 
-    
+	
