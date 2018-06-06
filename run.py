228d6e199e55edd6217279b79f08bdf866ae33b6
RachaelNantale <rachaelminani42@gmail.com>
2018-06-06 12:39:28 +0300

    Refactor code

--- run.py
+++ run.py
@@ -1,14 +1,12 @@
 from flask import Flask, request, jsonify,abort
-from api.views import my_app
 import json
 import api
 from api import *
 from api.views import app
-app.register_blueprint(my_app)
 
 
 
 if __name__ == '__main__':
-    app.run(debug='True')
+    app.run(debug='True', port='5003')
                 
                 
